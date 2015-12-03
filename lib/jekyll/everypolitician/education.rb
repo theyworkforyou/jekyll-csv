872d10b075c3ec6655a14933159393a0b9888b40
Chris Mytton <chrism@mysociety.org>
2015-12-03 14:30:47 +0000

    Update to allow multiple configurations

--- lib/jekyll/everypolitician/education.rb
+++ lib/jekyll/everypolitician/education.rb
@@ -11,13 +11,16 @@ module Jekyll
     module Education
       class Generator < ::Jekyll::Generator
         def generate(site)
-          csv_string = open(site.config['everypolitician']['education']).read
-          site.data['education'] = CSV.parse(csv_string, headers: true).map(&:to_hash)
-          site.collections['people'].docs.each do |person|
-            kuvakazim_identifier = person.data['identifiers'].find { |id| id['scheme'] == 'kuvakazim' }
-            next unless kuvakazim_identifier
-            kuvakazim_id = kuvakazim_identifier['identifier']
-            person.data['education_history'] = site.data['education'].find_all { |e| e['id'] == kuvakazim_id }
+          return unless site.config['remote_csv']
+          site.config['remote_csv'].each do |collection, config|
+            csv_string = open(config['source']).read
+            site.data[collection] = CSV.parse(csv_string, headers: true).map(&:to_hash)
+            # site.collections['people'].docs.each do |person|
+            #   kuvakazim_identifier = person.data['identifiers'].find { |id| id['scheme'] == 'kuvakazim' }
+            #   next unless kuvakazim_identifier
+            #   kuvakazim_id = kuvakazim_identifier['identifier']
+            #   person.data['education_history'] = site.data['education'].find_all { |e| e['id'] == kuvakazim_id }
+            # end
           end
         end
       end
--- test/jekyll/everypolitician/education_test.rb
+++ test/jekyll/everypolitician/education_test.rb
@@ -6,26 +6,35 @@ class Jekyll::Everypolitician::EducationTest < Minitest::Test
   end
 
   def site
-    @site ||= Jekyll::Site.new(Jekyll.configuration).tap do |s|
-      s.config['everypolitician'] ||= {}
-      s.config['everypolitician']['sources'] = ['test/fixtures/ep-popolo-v1.0.json']
-      s.config['everypolitician']['education'] = 'test/fixtures/education.csv'
-    end
+    @site ||= Jekyll::Site.new(Jekyll.configuration)
   end
 
   def setup
-    site.generate
+    site.config['remote_csv'] ||= {}
+    site.config['remote_csv']['education'] = {
+      'source' => 'test/fixtures/education.csv'
+    }
   end
 
   def test_it_populates_site_data_with_education
+    site.generate
     refute_nil site.data['education']
   end
 
   def test_the_correct_number_of_records_exist
+    site.generate
     assert_equal 9, site.data['education'].size
   end
 
   def test_records_are_parse_correctly
+    site.generate
     assert_equal 'Edward G. Cross', site.data['education'].first['name']
+  end
+
+  def test_adding_another_collection
+    site.config['remote_csv']['foo_bar'] = site.config['remote_csv']['education']
+    site.generate
+    assert_equal 9, site.data['education'].size
+    assert_equal 9, site.data['foo_bar'].size
   end
 end
